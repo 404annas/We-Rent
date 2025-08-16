e04f130564f75ea0f582353bc432117d9ebf89ad
Muhammad Annas Khan <annasking601@gmail.com>
2025-08-17 03:00:26 +0500

    Phase 1 Completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require("method-override");
 const Listing = require("./models/listings.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/werent";
@@ -17,6 +18,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("This is Home Route");
@@ -28,14 +30,40 @@ app.get("/listings", async (req, res) => {
   res.render("./listings/index.ejs", { allListings });
 });
 
+app.get("/listings/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+
 app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("./listings/show.ejs", { listing });
 });
 
-app.get("/listings/new", (req, res) => {
-  res.render("./listings/new.ejs");
+app.post("/listings", async (req, res) => {
+  let newListing = new Listing(req.body.listing);
+  // console.log(newListing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
 });
 
 // app.get("/testlisting", async (req,res) => {
